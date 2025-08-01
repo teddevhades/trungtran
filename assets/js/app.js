2a19fb9a3b36c48340976e615e5a87e8723abb0f
Tran Trung <tmttranminhtrung@gmail.com>
2025-08-01 21:41:39 +0700

    update home --2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 //[TMT]      script cho intro
-
 let intro = document.querySelector(".intro");
 let welcome__text = document.querySelector(".welcome__header");
 let welcome__text__span = document.querySelectorAll(".welcome__text");
+const navbar = document.querySelector(".navbar");
 window.addEventListener("DOMContentLoaded", () => {
   welcome__text__span.forEach((span, index) => {
     setTimeout(() => {
@@ -21,7 +21,11 @@ window.addEventListener("DOMContentLoaded", () => {
       }, 1800);
       setTimeout(() => {
         intro.style.top = "-100vh";
-      }, 2300);
+
+        if (navbar) {
+          navbar.classList.remove("navbar__hidden");
+        }
+      }, 2400);
     });
   });
 });
